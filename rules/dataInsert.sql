7e0c22d83d6d18524dd6c3278873043df4014d6e
Focus0Wizard <jhonn.ramirez@ucb.edu.bo>
2024-12-04 12:24:26 -0400

    indexeado

--- rules/dataInsert.sql
+++ rules/dataInsert.sql
@@ -1,5 +1,10 @@
 SELECT 
-current.state.reported.tempState AS tempState, current.state.reported.luxState AS luxState, current.state.reported.mode AS mode, 
-timeStamp() AS timestamp, topic(3) AS thing_name, substring(topic(3), 11) AS sn, current.state.reported.mode AS mode 
-FROM '$aws/things/+/shadow/update/documents' WHERE subString(topic(3), 0, 10) = 'greenhouse' AND current.state.reported.tempState >=0 AND 
-current.state.reported.tempState <=1 AND current.state.reported.mode >= 0 AND current.state.reported.mode <= 1
+current.state.reported.tempState AS tempState, current.state.reported.luxState AS luxState,
+current.state.reported.mode AS mode, 
+timeStamp() AS timestamp, topic(3) AS thing_name, substring(topic(3), 11) AS sn, 
+current.state.reported.mode AS mode 
+FROM '$aws/things/+/shadow/update/documents'
+WHERE subString(topic(3), 0, 10) = 'greenhouse' AND current.state.reported.tempState >=0 
+AND current.state.reported.tempState <=1 AND current.state.reported.mode >= 0 
+AND current.state.reported.mode <= 1
+
--- rules/repost.sql
+++ rules/repost.sql
@@ -2,5 +2,8 @@ SELECT current.state.reported.tempState AS tempState, current.state.reported.lux
 current.state.reported.mode AS mode, current.state.reported.irrigation_state AS irrigation_state, 
 current.state.reported.vent_state AS vent_state 
 FROM '$aws/things/+/shadow/update/documents'
-WHERE current.state.reported.tempState >= 0 AND current.state.reported.tempState <= 1 AND current.state.reported.luxState >= 0
+WHERE current.state.reported.tempState >= 0 
+AND current.state.reported.tempState <= 1 
+AND current.state.reported.luxState >= 0
 AND current.state.reported.luxState <= 1 AND current.state.reported.mode = 0
+
