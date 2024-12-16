1a0e09b27e9ce8db84d9b90fc9bd3403f9df604c
Ichurri <santiagoiturrivargas04@gmail.com>
2024-12-16 11:47:46 -0400

    remove wifimanager class from code

--- esp32/MQTTClient.h
+++ esp32/MQTTClient.h
@@ -1,104 +1,108 @@
 #ifndef MQTTCLIENT_H
 #define MQTTCLIENT_H
 
+#include <WiFiManager.h>        // Biblioteca para gestionar WiFi
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-class MQTTClient
-{
+class MQTTClient {
 private:
-  PubSubClient *client;
-  WiFiClientSecure wifiClient;
-  String shadowTopicUpdate;
-  String shadowTopicAccepted;
+    PubSubClient client;
+    WiFiClientSecure wifiClient;
+    String shadowTopicUpdate;
+    String shadowTopicAccepted;
 
-  int prevLdrState;
-  int prevDhtState;
+    int prevLdrState;
+    int prevDhtState;
 
-  void (*userCallback)(int luxState, int tempState);
+    void (*userCallback)(int luxState, int tempState);
 
-  static void internalCallback(char *topic, byte *payload, unsigned int length)
-  {
-    MQTTClient *instance = (MQTTClient *)PubSubClient::getDefaultCallbackObject();
-    if (!instance) return;
+    static void internalCallback(char* topic, byte* payload, unsigned int length) {
+        MQTTClient* instance = static_cast<MQTTClient*>(PubSubClient::getDefaultCallbackObject());
+        if (!instance) return;
 
-    instance->handleCallback(topic, payload, length);
-  }
-
-  void handleCallback(char *topic, byte *payload, unsigned int length)
-  {
-    String message;
-    for (unsigned int i = 0; i < length; i++) message += (char)payload[i];
-
-    DynamicJsonDocument doc(1024);
-    DeserializationError err = deserializeJson(doc, message);
-    if (err) return;
-
-    if (!doc["state"]["reported"].isNull())
-    {
-      JsonObject state = doc["state"]["reported"];
-
-      int luxState = state["luxState"] | -1;
-      int tempState = state["tempState"] | -1;
-
-      if (luxState != -1 && tempState != -1 && userCallback) userCallback(luxState, tempState);
+        instance->handleCallback(topic, payload, length);
     }
-  }
+
+    void handleCallback(char* topic, byte* payload, unsigned int length) {
+        String message;
+        for (unsigned int i = 0; i < length; i++) message += (char)payload[i];
+
+        DynamicJsonDocument doc(1024);
+        DeserializationError err = deserializeJson(doc, message);
+        if (err) return;
+
+        if (!doc["state"]["reported"].isNull()) {
+            JsonObject state = doc["state"]["reported"];
+
+            int luxState = state["luxState"] | -1;
+            int tempState = state["tempState"] | -1;
+
+            if (luxState != -1 && tempState != -1 && userCallback) userCallback(luxState, tempState);
+        }
+    }
 
 public:
-  MQTTClient(const char *broker, int port, const char *rootCA, const char *cert, const char *key, const char *clientId)
-      : prevLdrState(-1), 
-        prevDhtState(-1), 
-        userCallback(nullptr)
-  {
-    wifiClient.setCACert(rootCA);
-    wifiClient.setCertificate(cert);
-    wifiClient.setPrivateKey(key);
-    client = new PubSubClient(wifiClient);
-    client->setServer(broker, port);
-    PubSubClient::setDefaultCallbackObject(this);
+    MQTTClient(const char* broker, int port, const char* rootCA, const char* cert, const char* key)
+        : prevLdrState(-1), prevDhtState(-1), userCallback(nullptr) {
+        wifiClient.setCACert(rootCA);
+        wifiClient.setCertificate(cert);
+        wifiClient.setPrivateKey(key);
+        client.setClient(wifiClient);
+        client.setServer(broker, port);
+        PubSubClient::setDefaultCallbackObject(this);
 
-    shadowTopicUpdate = String("$aws/things/greenhouse_0001/shadow/update");
-    shadowTopicAccepted = String("$aws/things/greenhouse_0001/shadow/update/accepted");
-  }
-
-  void setCallback(void (*cb)(int, int))
-  {
-    userCallback = cb;
-    client->setCallback(internalCallback);
-  }
-
-  bool connect(const char *clientId)
-  {
-    while (!client->connected())
-    {
-      if (client->connect(clientId)) client->subscribe(shadowTopicAccepted.c_str());
-      else delay(5000);
+        shadowTopicUpdate = "$aws/things/greenhouse_0001/shadow/update";
+        shadowTopicAccepted = "$aws/things/greenhouse_0001/shadow/update/accepted";
     }
-    return true;
-  }
 
-  void loop()
-  {
-    client->loop();
-  }
+    void connectWiFi() {
+        WiFiManager wifiManager;
+        wifiManager.autoConnect("GreenhouseConfigAP");
+        Serial.println("Connected Wifi");
+    }
 
-  void publishState(int luxState, int tempState)
-  {
-    if (luxState == prevLdrState && tempState == prevDhtState) return;
-    
-    prevLdrState = luxState;
-    prevDhtState = tempState;
+    void setCallback(void (*cb)(int, int)) {
+        userCallback = cb;
+        client.setCallback(internalCallback);
+    }
 
-    StaticJsonDocument<200> jsonDoc;
-    jsonDoc["state"]["reported"]["luxState"] = luxState;
-    jsonDoc["state"]["reported"]["tempState"] = tempState;
+    bool connect(const char* clientId) {
+        while (!client.connected()) {
+            if (client.connect(clientId)) {
+                Serial.println("Connected to MQTT");
+                client.subscribe(shadowTopicAccepted.c_str());
+            } else {
+                Serial.print("Error MQTT connection, rc=");
+                Serial.println(client.state());
+                delay(5000);
+            }
+        }
+        return true;
+    }
 
-    char jsonBuffer[512];
-    serializeJson(jsonDoc, jsonBuffer);
-    client->publish(shadowTopicUpdate.c_str(), jsonBuffer);
-  }
+    void loop() {
+        client.loop();
+    }
+
+    void publishState(int luxState, int tempState) {
+        if (luxState == prevLdrState && tempState == prevDhtState) return;
+
+        prevLdrState = luxState;
+        prevDhtState = tempState;
+
+        StaticJsonDocument<200> jsonDoc;
+        jsonDoc["state"]["reported"]["luxState"] = luxState;
+        jsonDoc["state"]["reported"]["tempState"] = tempState;
+
+        char jsonBuffer[512];
+        serializeJson(jsonDoc, jsonBuffer);
+        client.publish(shadowTopicUpdate.c_str(), jsonBuffer);
+
+        Serial.println("Shadow published:");
+        Serial.println(jsonBuffer);
+    }
 };
 
 #endif
